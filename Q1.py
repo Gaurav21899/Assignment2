a5c8db4345554fa6317144e899ec2a16acab2773
Gaurav21899 <gaurav21899@gmail.com>
2023-09-15 10:54:52 +0530

    Declare a List and removing and element

---
+++ Q1.py
@@ -0,0 +1,16 @@
+# Declare a list variable
+cities = ["Mumbai", "Ayodhya","Jaipur", "Pune", "Nashik"]
+print(cities)
+#  Print the length of the list
+print(len(cities))
+
+# Append a new city to the existing list
+cities.append("Chennai")
+print(cities)
+#  Print the length of the list
+print(len(cities))
+
+# Remove an element at index '2’ from the list and print
+cities.pop(2)
+print(cities)
+print("Process Completed)
