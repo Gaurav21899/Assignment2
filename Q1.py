be0122bbde08675a17fda5f6d60bc75526fc59af
Gaurav21899 <gaurav21899@gmail.com>
2023-09-19 21:23:56 +0530

    Declare a List variable

--- Q1.py
+++ Q1.py
@@ -2,15 +2,17 @@
 cities = ["Mumbai", "Ayodhya","Jaipur", "Pune", "Nashik"]
 print(cities)
 #  Print the length of the list
-print(len(cities))
+print("Length of the Cities:",len(cities))
 
 # Append a new city to the existing list
 cities.append("Chennai")
 print(cities)
 #  Print the length of the list
-print(len(cities))
+print("Length of the Cities:", len(cities))
 
 # Remove an element at index '2’ from the list and print
 cities.pop(2)
 print(cities)
-print("Process Completed)
+print("Process Completed")
+
+
